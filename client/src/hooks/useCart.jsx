244215527d3b2f97e2feeb7ba817b7e788e8f719
Holo Oluwatosin Kehinde <holooluwatosin@gmail.com>
2023-12-04 17:23:24 +0100

    cart icon setup to dynamically catch items

--- client/src/hooks/useCart.jsx
+++ client/src/hooks/useCart.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import { useContext } from "react"
+import { AuthContext } from "../contexts/AuthProvider"
+import { useQuery } from "@tanstack/react-query"
 
 const useCart = () => {
-  return (
-    <div>useCart</div>
-  )
+    const { user } = useContext(AuthContext)
+    const { refetch, data: cart = []} = useQuery({
+      // tanstack Usage >> Quick function >> with fetch and other clients that do not throw by default
+      queryKey: ['carts', user?.email],
+      queryFn: async () => {
+        const response = await fetch(`http://localhost:6001/cart?email=${user?.email}`)
+        return response.json()
+      },
+    })
+
+  return [cart, refetch]
 }
 
 export default useCart
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -6,19 +6,27 @@ import { RouterProvider } from 'react-router-dom'
 import router from './router/Router.jsx'
 import AuthProvider from './contexts/AuthProvider.jsx'
 
+// tanStack Query-state mngt
+import {
+  useQuery,
+  useMutation,
+  useQueryClient,
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
+// Create a client
+const queryClient = new QueryClient()
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
   //   <App />
   // </React.StrictMode>,
 
-  // <AuthProvider>
-  //   <QueryClientProvider client={queryClient}>
-  //     <RouterProvider router={router} />
-  //   </QueryClientProvider>
-  // </AuthProvider>
-
   <AuthProvider>
-    <RouterProvider router={router} />
+    {/* From tanstack */}
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
   </AuthProvider>
 
 )
